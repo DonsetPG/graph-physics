47077eab08c52c729aeeefeb617bdf82059a6efc
Vincent Lannelongue <vincent.lannelongue@minesparis.psl.eu>
2025-06-19 17:40:10 +0200

    Add retrain script to test resume_training behaviour in CI/CD

---
+++ retrain.sh
@@ -0,0 +1,14 @@
+python -m graphphysics.train \
+            --training_parameters_path=mock_training.json \
+            --num_epochs=2 \
+            --init_lr=0.001 \
+            --batch_size=1 \
+            --warmup=500 \
+            --num_workers=0 \
+            --prefetch_factor=0 \
+            --model_save_name=model \
+            --resume_training=True \
+            --model_path=checkpoints/model.ckpt \
+            --no_edge_feature \
+            --resume_training=True \
+            --model_path=checkpoints/model.ckpt
