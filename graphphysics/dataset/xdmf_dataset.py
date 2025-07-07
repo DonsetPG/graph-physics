270532e89875807ee72c867f41178cb17c4e92f2
Clément Hervé <clement.herve@etu.minesparis.psl.eu>
2025-07-07 15:29:14 +0900

    fix: npz keys

--- graphphysics/dataset/xdmf_dataset.py
+++ graphphysics/dataset/xdmf_dataset.py
@@ -71,8 +71,8 @@ class XDMFDataset(BaseDataset):
         file_name = file_name.replace(".xdmf", ".npz")
 
         data_npz = np.load(file_name)
-        feats = data_npz["feats"]
-        coords = data_npz["coords"]
+        feats = data_npz["indices"]
+        coords = data_npz["patchtokens"]
 
         return feats, coords
 
