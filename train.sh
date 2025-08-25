c31769ca5171f813d49335594cf5ee0d92a4df4d
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2025-08-25 10:45:22 +0200

    temp

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 python -m graphphysics.train \
-            --training_parameters_path=mock_training.json \
-            --num_epochs=1 \
+            --training_parameters_path=training_config/coarse-aneurysm.json \
+            --num_epochs=40 \
             --init_lr=0.001 \
             --batch_size=1 \
-            --warmup=500 \
-            --num_workers=0 \
-            --prefetch_factor=0 \
+            --warmup=1500 \
+            --num_workers=2 \
+            --prefetch_factor=2 \
             --model_save_name=model \
             --no_edge_feature
