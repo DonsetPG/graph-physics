f5c9b43c5294b32b1351c5330ff87e5db292d9aa
Théodore Michel <75378484+theodore-michel@users.noreply.github.com>
2025-10-08 09:11:36 +0200

    feat: pass prediction save path as argument

--- predict.sh
+++ predict.sh
@@ -1,4 +1,5 @@
 python -m graphphysics.predict \
             --predict_parameters_path=mock_training.json \
             --model_path=checkpoints/model.ckpt \
-            --no_edge_feature
+            --no_edge_feature \
+            --prediction_save_path=predictions \
