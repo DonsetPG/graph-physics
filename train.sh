e1427904864ac5777599d2540b1873cc1df841bc
hadriencalmet51 <hadriencalmet@gmail.com>
2025-10-21 12:27:26 +0200

    I forgot the train.sh

--- train.sh
+++ train.sh
@@ -1,10 +1,15 @@
+echo "=== GPU INFO ==="
+nvidia-smi
+echo "================"
+
 python -m graphphysics.train \
-            --training_parameters_path=mock_training.json \
+            --training_parameters_path=nose_training.json \
             --num_epochs=1 \
             --init_lr=0.001 \
             --batch_size=1 \
             --warmup=500 \
             --num_workers=0 \
             --prefetch_factor=0 \
-            --model_save_name=model \
-            --no_edge_feature
+            --model_save_name=model.ckpt \
+            --no_edge_feature \
+	    --use_previous_data=false
