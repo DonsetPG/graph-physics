5676c0540fa0eeab310e5847a46fa0bcea6e40be
Vincent Lannelongue <vincent.lannelongue@minesparis.psl.eu>
2026-01-30 11:24:54 +0100

    Add stent features to aneurysm build_features and set params to local training setup

--- graphphysics/external/aneurysm.py
+++ graphphysics/external/aneurysm.py
@@ -5,6 +5,12 @@ from graphphysics.utils.nodetype import NodeType
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+STENTS = {
+    "S1": {"wire_number": 36, "wire_radius": 0.015, "wire_distance": 0.358},
+    "S2": {"wire_number": 24, "wire_radius": 0.015, "wire_distance": 0.540},
+    "S3": {"wire_number": 16, "wire_radius": 0.015, "wire_distance": 0.805},
+}
+
 
 def aneurysm_node_type(graph: Data) -> torch.Tensor:
     v_x = graph.x[:, 0]
@@ -24,8 +30,22 @@ def aneurysm_node_type(graph: Data) -> torch.Tensor:
     return node_type.to(device)
 
 
+def build_stent_features(graph: Data, node_type: torch.Tensor) -> Data:
+
+    stent_id = graph.id.split('_')[-1]
+    stent_params = STENTS[stent_id]
+    stent = torch.Tensor([stent_params.values()], device=device)
+    stent_features = torch.zeros((graph.x.shape[0], 3), device=device)
+
+    stent_mask = node_type == NodeType.STENT
+    stent_features[stent_mask] = stent
+
+    return stent_features
+
+
 def build_features(graph: Data) -> Data:
-    node_type = aneurysm_node_type(graph)
+    node_type = graph.x[:, 3]
+    lvlset_inlet = graph.x[:, 4]
 
     current_velocity = graph.x[:, 0:3]
     target_velocity = graph.y[:, 0:3]
@@ -48,17 +68,31 @@ def build_features(graph: Data) -> Data:
         next_acceleration_unique
     )
 
+    if NodeType.STENT in torch.unique(node_type):
+        timestep = graph.x[:, 6]
+        lvlset_stent = graph.x[:, 5]
+        stent_features = build_stent_features(graph)
+    else:
+        timestep = graph.x[:, 5]
+        lvlset_stent = torch.zeros_like(lvlset_inlet)
+        stent_features = torch.zeros((graph.x.shape[0], 3), device=device)
+
     graph.x = torch.cat(
         (
-            graph.x,
+            current_velocity,
+            timestep.to(device).unsqueeze(1),
             acceleration,
             graph.pos,
             mean_next_accel.unsqueeze(1),
             min_next_accel.unsqueeze(1),
             max_next_accel.unsqueeze(1),
+            lvlset_inlet.to(device).unsqueeze(1),
+            lvlset_stent.to(device).unsqueeze(1),
+            stent_features,
             node_type.to(device).unsqueeze(1),
         ),
         dim=1,
     )
+    # print(graph.x[1000])
 
     return graph
--- train.sh
+++ train.sh
@@ -1,10 +1,13 @@
 python -m graphphysics.train \
-            --training_parameters_path=mock_training.json \
-            --num_epochs=1 \
+            --project_name=AXC \
+            --training_parameters_path=training_config/coarse-aneurysm.json \
+            --num_epochs=40 \
             --init_lr=0.001 \
-            --batch_size=1 \
-            --warmup=500 \
-            --num_workers=0 \
-            --prefetch_factor=0 \
+            --batch_size=2 \
+            --warmup=1500 \
+            --num_workers=2 \
+            --prefetch_factor=2 \
             --model_save_name=model \
-            --no_edge_feature
+            --no_edge_feature \
+            --use_previous_data=true \
+            --seed=1
