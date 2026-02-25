a960bc9e5881d2009c5fac487cda954bece339e6
Vincent Lannelongue <vincent.lannelongue@minesparis.psl.eu>
2026-02-25 10:47:18 +0100

    Add specific loss and noise for aneurysm nodetype

--- graphphysics/utils/loss.py
+++ graphphysics/utils/loss.py
@@ -75,6 +75,45 @@ class L2Loss(_Loss):
         return torch.mean(errors)
 
 
+class L2AneurysmLoss(_Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @property
+    def __name__(self):
+        return "AneurysmL2"
+
+    def forward(
+        self,
+        target: torch.Tensor,
+        network_output: torch.Tensor,
+        node_type: torch.Tensor,
+        masks: list[NodeType],
+        selected_indexes: torch.Tensor = None,
+        **kwargs
+    ) -> torch.Tensor:
+        """
+        Computes L2 loss for nodes of specific types.
+
+        Args:
+            target (torch.Tensor): The target values.
+            network_output (torch.Tensor): The predicted values from the network.
+            node_type (torch.Tensor): Tensor containing the type of each node.
+            masks (list[NodeType]): List of NodeTypes to include in the loss calculation.
+            selected_indexes (torch.Tensor, optional): Indexes of nodes to exclude from the loss calculation.
+
+        Returns:
+            torch.Tensor: The mean squared error for the specified node types.
+
+        Note:
+            This method calculates the L2 loss only for nodes of the types specified in 'masks'.
+            If 'selected_indexes' is provided, those nodes are excluded from the loss calculation.
+        """
+        mask = node_type == NodeType.ANEURYSM
+        errors = ((network_output - target) ** 2)[mask]
+        return torch.mean(errors)
+
+
 class CosineLoss(_Loss):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -484,6 +523,7 @@ class MultiLoss(_Loss):
 
 class LossType(enum.Enum):
     L2LOSS = L2Loss
+    L2ANEURYSMLOSS = L2AneurysmLoss
     COSINEL2LOSS = CosineLoss
     L1SMOOTHLOSS = L1SmoothLoss
     GRADIENTL2LOSS = GradientL2Loss
